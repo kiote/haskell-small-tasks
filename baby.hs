375acce049a8891410bf528e639f2b1b0d5c3ca3
Ekaterina Krivich <ekaterinak@heathmont.net>
2019-04-25 09:30:13 +0300

    add maximim

--- baby.hs
+++ baby.hs
@@ -29,3 +29,10 @@ cylinder r h =
     let sideArea = 2*pi*r*h
         topArea = pi*r^2
     in sideArea + 2*topArea
+maximum' :: (Ord a) => [a] -> a
+maximum' [] = error "maximum of empty list"
+maximum' [x] = x
+maximum' (x:xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where maxTail = maximum' xs
